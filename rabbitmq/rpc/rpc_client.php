e175063b812fe78f88e32e32fcd9396d6922f038
wuzhc <1716220125@qq.com>
2017-09-30 17:49:31 +0800

    rabbitmq demo

--- rabbitmq/rpc/rpc_client.php
+++ rabbitmq/rpc/rpc_client.php
@@ -5,3 +5,65 @@
  * Date: 2017年09月30日
  * Time: 11:50
  */
+
+require_once '../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class FibonacciRpcClient
+{
+    private $connection;
+    private $channel;
+    private $callback_queue;
+    private $response;
+    private $corr_id;
+
+    public function __construct()
+    {
+        $this->connection = new AMQPStreamConnection(
+            'localhost', 5672, 'guest', 'guest');
+        $this->channel = $this->connection->channel();
+        list($this->callback_queue, ,) = $this->channel->queue_declare(
+            "", false, false, true, false);
+        $this->channel->basic_consume(
+            $this->callback_queue, '', false, false, false, false,
+            array($this, 'on_response'));
+    }
+
+    public function on_response($rep)
+    {
+        if ($rep->get('correlation_id') == $this->corr_id) {
+            $this->response = $rep->body;
+        }
+    }
+
+    public function call($n)
+    {
+        $this->response = null;
+        $this->corr_id = uniqid();
+
+        $msg = new AMQPMessage(
+            json_encode(['num' => $n]),
+            array(
+                'correlation_id' => $this->corr_id,
+                'reply_to' => $this->callback_queue,
+                'content_type' => 'application/json',
+            )
+        );
+        $this->channel->basic_publish($msg, '', 'rpc_queue');
+        while (!$this->response) {
+            $this->channel->wait();
+        }
+        return intval($this->response);
+    }
+}
+
+
+if (!isset($argv[1]) || empty($argv[1]) || !is_numeric($argv[1])) {
+    file_put_contents('php://stderr', "Usage: $argv[0] [num]");
+    exit(1);
+}
+
+$fibonacci_rpc = new FibonacciRpcClient();
+$response = $fibonacci_rpc->call($argv[1]);
+echo " [.] Got ", $response, "\n";
--- rabbitmq/rpc/rpc_server.php
+++ rabbitmq/rpc/rpc_server.php
@@ -1,7 +1,65 @@
 <?php
 /**
+ * RPC优势，
+ * （1） 如果rpc_server忙的时候，可以多开几个server
+ *
+ * 如果rpc_server没启动，怎么办？？
+ * 客户端需要设置超时时间？
+ * 如果rpc_server异常，是否返回给客户端？？
+ *
+ *
  * Created by PhpStorm.
  * User: wuzhc
  * Date: 2017年09月30日
  * Time: 11:50
  */
+
+require_once '../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('127.0.0.1', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+// 声明队列
+$channel->queue_declare('rpc_queue', false, false, false, false);
+
+function fib($n) {
+    if ($n == 0)
+        return 0;
+    if ($n == 1)
+        return 1;
+    return fib($n-1) + fib($n-2);
+}
+
+// 回调
+$callback = function ($req) {
+    $data = json_decode($req->body, true);
+    $num = $data['num'];
+    $timeout = isset($data['timeout']) ? $data['timeout'] : 0;
+    $now = time();
+
+    // 处理消息
+    echo " [x] fib($num)" . PHP_EOL;
+
+    $result = (string)fib($num);
+
+    // $msg
+    $msg = new AMQPMessage($result, array(
+        'correlation_id' => $req->get('correlation_id')
+    ));
+
+    $req->delivery_info['channel']->basic_publish($msg, '', $req->get('reply_to'));
+    $req->delivery_info['channel']->basic_ack($req->delivery_info['delivery_tag']);
+};
+
+echo " [x] Awaiting RPC requests\n";
+$channel->basic_qos(null, 1, null);
+$channel->basic_consume('rpc_queue', '', false, false, false, false, $callback);
+
+while (count($channel->callbacks)) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
