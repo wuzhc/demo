be852bb734cba604076c37489d53e3c6edb042c4
wuzhc <1716220125@qq.com>
2017-12-06 14:48:28 +0800

    signal exercise

--- swoole/process.php
+++ swoole/process.php
@@ -1,38 +1,34 @@
 <?php
+include "../log.php";
 
 if ($argc < 2 || $argv[1] == '--help') {
     printf("Usage: %s num \n", $argv[0]);
     exit;
 }
 
+$pids = [];
+
 for ($i = 0; $i < $argv[1]; $i++) {
     $process = new swoole_process(function (swoole_process $process) use ($i) {
         $process->name('child_process_' . $i); // 为进程命名，是swoole_set_process_name的别名
         // do something
-        $sleep = rand(1,10);
-        sleep($sleep);
-        $process->write("hello world $i \n");
+        log::w("child_process_$i");
+//        $process->write("hello world $i \n");
         $process->exit(0); // 0表示正常退出
     });
-    $pid = $process->start(); // 执行fork调用，成功返回pid，失败返回false
-    if (false == $pid) {
-        exit("fork failed \n");
-    }
+    $pids[] = $process->start(); // 执行fork调用，成功返回pid，失败返回false
 }
 
 // 安装sigchld信号处理器
-//        swoole_process::signal(SIGCHLD, function ($sig) {
-//            //必须为false，非阻塞模式（非阻塞 + signal异步处理）
-//            while ($ret = swoole_process::wait(false)) { // 回收子进程，否则子进程会变成僵尸进程浪费资源
-//                echo "PID={$ret['pid']}\n";
-//            }
-//        });
+swoole_process::signal(SIGCHLD, function ($sig) use (&$pids) {
+    //必须为false，非阻塞模式（非阻塞 + signal异步处理）
+    while ($ret = swoole_process::wait(false)) { // 回收子进程，否则子进程会变成僵尸进程浪费资源
+        echo "PID={$ret->pid}\n";
+        unset($pids[$ret->pid]);
+    }
+});
 
-while ($data = $process->read()) {
-    echo "has data \n";
-    print_r($data);
+while (!empty($pids)) {
+    print_r($pids);
 }
-
-while ($ret = swoole_process::wait(true)) {
-    echo "PID={$ret['pid']}\n";
-}
+echo "nothing \n";
