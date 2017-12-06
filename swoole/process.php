718f3a534e32e1c2d1e807a7d54c78f8a3f565e4
wuzhc <1716220125@qq.com>
2017-12-06 15:28:02 +0800

    process exercise

--- swoole/process.php
+++ swoole/process.php
@@ -6,7 +6,7 @@ if ($argc < 2 || $argv[1] == '--help') {
     exit;
 }
 
-$pids = [];
+$data = [];
 
 for ($i = 0; $i < $argv[1]; $i++) {
     $process = new swoole_process(function (swoole_process $process) use ($i) {
@@ -15,7 +15,8 @@ for ($i = 0; $i < $argv[1]; $i++) {
         sleep(3);
         $process->exit(0); // 0表示正常退出
     });
-    $pids[] = $process->start(); // 执行fork调用，成功返回pid，失败返回false
+    $pid = $process->start(); // 执行fork调用，成功返回pid，失败返回false
+    $data[$pid] = 1;
 }
 
 // 安装sigchld信号处理器
@@ -29,22 +30,22 @@ for ($i = 0; $i < $argv[1]; $i++) {
 //});
 
 // 安装sigchld信号处理器
-swoole_process::signal(SIGCHLD, function ($sig) use (&$pids) {
+swoole_process::signal(SIGCHLD, function ($sig) use (&$data) {
 //    必须为false，非阻塞模式（非阻塞 + signal异步处理）
     while ($ret = swoole_process::wait(false)) { // 回收子进程，否则子进程会变成僵尸进程浪费资源
         echo is_bool($ret) ? $ret ? 'true' : 'false' : 'no bool';
         echo PHP_EOL;
         echo "PID={$ret['pid']}\n";
-        unset($pids[$ret['pid']]);
+        unset($data[$ret['pid']]);
     }
-    if (empty($pids)) {
+    if (empty($data)) {
         exit(0);
     } else {
-        var_dump($pids);
+        var_dump($data);
     }
 });
 
-foreach ($pids as $pid) {
+foreach ($data as $pid => $v) {
     echo "process $pid \n";
 }
 
