18eba9cf79385293577a9b17201f2633fcef9c2c
wuzhc <1716220125@qq.com>
2017-12-06 15:28:41 +0800

    process exercise

--- swoole/process.php
+++ swoole/process.php
@@ -12,7 +12,7 @@ for ($i = 0; $i < $argv[1]; $i++) {
     $process = new swoole_process(function (swoole_process $process) use ($i) {
         $process->name('child_process_' . $i); // 为进程命名，是swoole_set_process_name的别名
         log::w("child_process_$i");
-        sleep(3);
+//        sleep(3);
         $process->exit(0); // 0表示正常退出
     });
     $pid = $process->start(); // 执行fork调用，成功返回pid，失败返回false
