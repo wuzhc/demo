6c690d1d239af68e2c88aa246b64f69c24f61c49
wuzhc <1716220125@qq.com>
2017-12-06 15:10:33 +0800

    process exercise

--- swoole/process.php
+++ swoole/process.php
@@ -40,7 +40,7 @@ echo "waiting... \n";
 // 安装sigchld信号处理器
 swoole_process::signal(SIGCHLD, function ($sig) use (&$pids) {
 //    必须为false，非阻塞模式（非阻塞 + signal异步处理）
-    while ($ret = swoole_process::wait(false)) { // 回收子进程，否则子进程会变成僵尸进程浪费资源
+    while ($ret = swoole_process::wait(true)) { // 回收子进程，否则子进程会变成僵尸进程浪费资源
         var_dump($ret);
         echo "PID={$ret['pid']}\n";
 //        unset($pids[$ret['pid']]);
