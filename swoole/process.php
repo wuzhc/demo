b15670899a61c9ba411ff12318dd011ca055bc24
wuzhc <1716220125@qq.com>
2017-12-06 15:16:28 +0800

    process exercise

--- swoole/process.php
+++ swoole/process.php
@@ -12,7 +12,7 @@ for ($i = 0; $i < $argv[1]; $i++) {
     $process = new swoole_process(function (swoole_process $process) use ($i) {
         $process->name('child_process_' . $i); // 为进程命名，是swoole_set_process_name的别名
         log::w("child_process_$i");
-        sleep(10);
+        sleep(3);
         $process->exit(0); // 0表示正常退出
     });
     $pids[] = $process->start(); // 执行fork调用，成功返回pid，失败返回false
@@ -38,12 +38,10 @@ echo "waiting... \n";
 //swoole_process::wait(true);
 
 // 安装sigchld信号处理器
-swoole_process::signal(SIGCHLD, function ($sig) use (&$pids) {
+swoole_process::signal(SIGCHLD, function ($sig) {
 //    必须为false，非阻塞模式（非阻塞 + signal异步处理）
     while ($ret = swoole_process::wait(false)) { // 回收子进程，否则子进程会变成僵尸进程浪费资源
-        var_dump($pids);
         echo "PID={$ret['pid']}\n";
-        unset($pids[$ret['pid']]);
     }
 });
 
