8f57014fe78c24c79f496639d180183e2756eb44
wuzhc <1716220125@qq.com>
2017-12-07 11:04:20 +0800

    swoole wait children process

---
+++ swoole/wait.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wuzhc
+ * Date: 2017年12月07日
+ * Time: 10:40
+ */
+
+$pid = posix_getgid();
+echo "parent pid = $pid \n";
+
+swoole_process::signal(SIGCHLD, function($sig){
+    echo "caught signal $sig \n";
+});
+
+for ($i = 0; $i < 5; $i++) {
+    $process = new swoole_process(function (swoole_process $process) {
+        echo "child pid = $process->pid \n";
+        $process->exit(0);
+    });
+}
+
+$total = 0;
+while ($ret = swoole_process::wait(false)) { // 回收子进程，否则子进程会变成僵尸进程浪费资源
+    $total++;
+}
+echo "$total child process exit";
+exit;
+
